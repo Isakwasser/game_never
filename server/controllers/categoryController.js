a6506ea46da0e5b6da21e2326b8e5262a696c81a
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-04-21 22:00:05 +0300

    Добавил сеттер категорий

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,4 +1,11 @@
+const { Category } = require('../models/models');
+
 class CategoryController {
+    async create(req, res) {
+        const { name, description } = req.body;
+        const category = await Category.create({ name, description });
+        return res.json(category);
+    }
     async getAll(req, res) {
 
     }
--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -2,7 +2,7 @@ const Router = require('express');
 const router = new Router();
 const categoryController = require('../controllers/categoryController');
 
-router.post('/',);
+router.post('/create', categoryController.create);
 router.get('/', categoryController.getAll);
 router.get('/:id', categoryController.getOne);
 
