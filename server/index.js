6e9320a990fd3f0421b6e36b768b09cdfe596fb0
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-04-21 20:12:02 +0300

    Создал роутер

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const sequelize = require('./db');
 const models = require('./models/models');
+const router = require('./routes/index');
 
 const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
+app.use('/api', router)
 
 const start = async () => {
     try {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const Router = require('express');
 const router = new Router();
+const categoryRouter = require('./categoryRouter');
+const questionRouter = require('./questionRouter');
 
-router.use('/category',);
-router.use('/question',);
+router.use('/category', categoryRouter);
+router.use('/question', questionRouter);
 
 module.exports = router;
