563fbf6128cc0903faaf23ff6938953f95864ccf
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-04-22 21:42:28 +0300

    обавил авторизацию по jwt

---
+++ server/controllers/userController.js
@@ -0,0 +1,51 @@
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { Admin } = require('../models/models');
+
+const generateJwt = (id, login) => {
+    return jwt.sign({ id, login },
+        process.env.SECRET_KEY,
+        { expiresIn: '24h' });
+}
+
+class UserController {
+    async registration(req, res, next) {
+        const { login, password } = req.body;
+        if (!login) {
+            return next(ApiError.badRequest('Укажите логин'));
+        }
+        if (!password) {
+            return next(ApiError.badRequest('Укажите пароль'));
+        }
+        const candidate = await Admin.findOne({
+            where: { login },
+        });
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким логином уже существует'));
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await Admin.create({
+            login,
+            password: hashPassword,
+        });
+        const token = generateJwt(user.id, login);
+        res.json({ token });
+
+    }
+    async login(req, res, next) {
+        const { login, password } = req.body;
+        const user = await Admin.findOne({ where: { login } });
+        if (!user) {
+            return next(ApiError.badRequest('Пользователя с таким логином не существует'));
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password);
+        if (!comparePassword) {
+            return next(ApiError.badRequest('Указан неверный пароль'));
+        }
+        const token = generateJwt(user.id, user.login);
+        return res.json({ token });
+    }
+}
+
+module.exports = new UserController();
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const Router = require('express');
 const router = new Router();
 const categoryRouter = require('./categoryRouter');
 const questionRouter = require('./questionRouter');
+const userRouter = require('./userRouter');
 
 router.use('/category', categoryRouter);
 router.use('/question', questionRouter);
+router.use('/user', userRouter);
 
 module.exports = router;
