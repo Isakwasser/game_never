8781021cac1f7df0b630032c251f6f78e3899a91
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-04-24 17:25:24 +0300

    change front fetch config

--- client/src/fetch/PlayFetch.js
+++ client/src/fetch/PlayFetch.js
@@ -2,6 +2,7 @@ import config from './config.js'
 
 function fetchPlay(options) {
     fetch(config.URL + '/question/getToPlay', {
+        mode: 'no-cors',
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
