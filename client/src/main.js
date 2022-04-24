9c0af44b03ab7d36ed00451671fab0a6c722f9d9
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-04-24 16:23:01 +0300

    Fix bug of 'require'

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/bootstrap/js/bootstrap.min.js'
 import './assets/bootstrap/css/bootstrap.min.css'
 import AlertView from './components/Alert/AlertView.vue'
 
