38e0597bf27c79b675fe3c7f306b80acf47ac0a1
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-04-27 00:25:03 +0300

    Добавил регистрацию только через админку

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,7 +3,7 @@ const router = new Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/registration', userController.registration);
+router.post('/registration', authMiddleware, userController.registration);
 router.post('/login', userController.login);
 router.post('/check', authMiddleware, userController.check);
 
