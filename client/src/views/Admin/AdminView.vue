0232851c1b974066eb45682b3beff500dfd9c816
Isakwasser <wasser18@bk.ru>
2022-05-31 00:37:24 +0300

    Fix admin view

--- client/src/views/Admin/AdminView.vue
+++ client/src/views/Admin/AdminView.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container-fluid">
-    <div class="row">
-      <div class="col-2">
+    <div class="row Admin__window flex-column flex-md-row">
+      <div class="col-md-2 bg-dark pt-3">
         <div v-if="!isLogin">
-          <router-link :to="{ name: 'login' }">Login</router-link>
+          <router-link :to="{ name: 'login' }" class="btn w-100 mb-3 btn-light" >Login</router-link>
         </div>
         <div v-else>
           <div>
-            <button @click="logout">Logout</button>
+            <button @click="logout" class="btn w-100 mb-3 btn-outline-light">Logout</button>
           </div>
           <div>
-            <router-link :to="{ name: 'questions' }">Вопросы</router-link>
+            <router-link :to="{ name: 'questions' }" class="btn w-100 mb-3 btn-outline-light" active-class="btn-light text-dark">Вопросы</router-link>
           </div>
           <div>
-            <router-link :to="{ name: 'categories' }">Категории</router-link>
+            <router-link :to="{ name: 'categories' }" class="btn w-100 mb-3 btn-outline-light" active-class="btn-light text-dark">Категории</router-link>
           </div>
         </div>
       </div>
-      <div class="col-10">
+      <div class="col-md-10 py-3">
         <router-view :setToken="setToken" :setInfo="setInfo" :token="token" />
       </div>
     </div>
